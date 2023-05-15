5658277f8e87c98c7664dde7531c08de5319470a
DembovychAnastasia <113368687+DembovychAnastasia@users.noreply.github.com>
2023-05-15 14:41:56 +0000

    Deploying to gh-pages from @ DembovychAnastasia/goit-react-hw-05-movies@af43b74d19b786afd4d1eca51fea6319c4ea8a68 🚀

--- static/js/134.66229d58.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[134],{134:function(e,t,c){c.r(t);var r=c(184);t.default=function(){return(0,r.jsx)("div",{children:"Movies colection"})}}}]);
-//# sourceMappingURL=134.66229d58.chunk.js.map
